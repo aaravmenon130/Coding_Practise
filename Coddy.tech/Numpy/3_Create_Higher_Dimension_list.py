dfaa24bc3c7c29e3066f623f9f5a3596f7498911
aaravmenon130 <aaravmenon130@gmail.com>
2025-10-11 20:24:34 +0530

    Create 3_Create_Higher_Dimension_list.py

---
+++ Coddy.tech/Numpy/3_Create_Higher_Dimension_list.py
@@ -0,0 +1,5 @@
+#Create a function named one_dimension_higher that receives a list and converts it to an array one dimension higher.
+import numpy as np
+def one_dimension_higher(lst):
+    #pass
+    return np.array([lst])
